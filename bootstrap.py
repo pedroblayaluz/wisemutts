ee7218d5474c3875372533c12227554908255aaa
Pedro Blaya Luz <blaya.luz@gmail.com>
2026-01-26 12:11:37 -0300

    fix imports at top

--- bootstrap.py
+++ bootstrap.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 import sys
 import os
+import awslambdaric
+from src.main import lambda_handler
 
 # Add lambda task root to path
 sys.path.insert(0, '/var/task')
 os.chdir('/tmp')
-
-import awslambdaric
-from src.main import lambda_handler
 
 if __name__ == "__main__":
     awslambdaric.run(lambda_handler)
